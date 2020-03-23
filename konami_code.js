68aaab9c74cce89e5b7b1264391e17e4bf116e4b
Ben Anderson <andersjbe@gmail.com>
2020-03-23 17:35:06 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,26 +13,14 @@ const codes = [
 
 var i = 0;
 
-function konamiListener(e) {
-  var key = e.key;
-  if (key === codes[i]) {
-    i++;
-    if(i === codes.length) {
-      alert("Code confirmed");
-      i = 0;
-    }
-  } else {
-    i = 0;
-  }
-}
-
 function init() {
-  document.body.addEventListener("keydown", e => {
+  // your code here
+  document.body.addEventListener(keydown, e => {
     var key = e.key;
     if (key === codes[i]) {
       i++;
       if(i === codes.length) {
-        alert("Code confirmed");
+        alert('Code confirmed');
         i = 0;
       }
     } else {
